c929d12c7b1c72c2c0fe4584124f0bf6831e0468
Joungseup Shin <jsshin@doubled.kr>
2023-11-08 16:04:29 +0900

    MobileSoftware - Java

---
+++ test_ota.py
@@ -0,0 +1,11 @@
+from ota import OTAUpdater
+#from WIFI_CONFIG import SSID, PASSWORD
+
+SSID = "DDPillow"
+PASSWORD = "dp299792*"
+
+firmware_url = "https://raw.githubusercontent.com/pierreyvesbaloche/kevinmca_ota/main/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+
+ota_updater.download_and_install_update_if_available()                                                                                                                                          
