1018aae903d00cedafcce2157d9f93758a4f8366
Joungseup Shin <jsshin@doubled.kr>
2023-11-08 18:02:41 +0900

    Print hi

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from ota import OTAUpdater
-#from WIFI_CONFIG import SSID, PASSWORD
-
-SSID = "DDPillow"
-PASSWORD = "dp299792*"
-
-firmware_url = "https://raw.githubusercontent.com/ota_test/"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-
-ota_updater.download_and_install_update_if_available()                                    
+print(hi)                                 
