cc3e2f4e460d9f5efb892730d0d2a29a7c83970c
Joungseup Shin <jsshin@doubled.kr>
2023-11-14 11:37:07 +0900

    feat: firmware 변경

--- firmware.py
+++ firmware.py
@@ -1,15 +1,1 @@
-from ota_module import ota
-from ota_module.ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
-
-FIRMWARE_URL = "https://raw.githubusercontent.com/JSShinnn/ota_test/main/"
-
-def ota_update(firmware_url, filename) :
-    # FIRMWARE_URL 폴더 내에 "firmware.py의 파일이 펌웨어 파일이 됨"
-
-    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, filename)
-
-    ota_updater.download_and_install_update_if_available()
-
-
-ota_update(FIRMWARE_URL, "firmware.py")
+print(hi)
